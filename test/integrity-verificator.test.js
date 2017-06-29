1f115745854b0f8375235e8c7d6ecf1c77605260
Rafael Castro <rccastrorafael@gmail.com>
2017-06-29 00:28:33 -0300

    :bug: fix moduleElevenBase9 bug and add another test

--- test/integrity-verificator.test.js
+++ test/integrity-verificator.test.js
@@ -2,8 +2,8 @@ const { moduleElevenBaseNine } = require('../lib/integrity-verificator')
 
 describe('moduleElevenBaseNine main functionality', () => {
   it('moduleElevenBaseNine should calculate verify number integrity, using module Eleven base Nine',() => {
-    const actual = moduleElevenBaseNine('1234567')
-    const expected = 9
+    const actual = moduleElevenBaseNine('2379000000000000004025196753360333600006000')
+    const expected = 3
     expect(actual).toEqual(expected)
   })
 })
