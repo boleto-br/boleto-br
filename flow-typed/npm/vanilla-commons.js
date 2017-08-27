f10b9f5ea924946f5de8cd9b52f6723a941d5575
Rafael Castro <rccastrorafael@gmail.com>
2017-08-26 23:24:36 -0300

    :spakles: created remittance file parser

--- flow-typed/npm/vanilla-commons.js
+++
@@ -1,3 +0,0 @@
-declare module 'vanilla-commons' {
-  declare function formatDate(format: string, date: Date): string
-}
---
+++ test/bradesco/parse-remittance-file.test.js
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import path from 'path'
+import parseRemittanceFile from '../../lib/bradesco/parse-remittance-file'
+
+describe('parseRemittanceFile main functionality', () => {
+  it('should parse a remittance file to a object', () => {
+    const file = fs.readFileSync(
+      path.join(__dirname, '/files/retorno.rem'),
+      'utf8'
+    )
+    const parsed = parseRemittanceFile(file)
+    const expected = {
+      account: '00169102',
+      agency: '01420',
+      card: '09',
+      company: 'ACME SA',
+      date: new Date(2012, 3, 11),
+      registerCode: '04146792000168',
+      bills: [
+        {
+          agency: '00523',
+          bank: '237',
+          creditDate: null,
+          maturity: new Date(2012, 3, 11),
+          occurency: '02',
+          occurencyDate: new Date(2012, 3, 11),
+          occurencyReasons: ['00', '00', '00', '00', '00'],
+          ourNumber: '00000000009',
+          paidValue: '0000000000000',
+          value: '0000000000500'
+        },
+        {
+          agency: '01420',
+          bank: '237',
+          creditDate: new Date(2012, 3, 13),
+          maturity: new Date(2012, 3, 11),
+          occurency: '06',
+          occurencyDate: new Date(2012, 3, 11),
+          occurencyReasons: ['00', '00', '00', '00', '00'],
+          ourNumber: '00000000009',
+          paidValue: '0000000000500',
+          value: '0000000000500'
+        }
+      ]
+    }
+
+    expect(parsed).toEqual(expected)
+  })
+})
