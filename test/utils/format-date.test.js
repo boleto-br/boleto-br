3f592650e3971ec1ea19b4d999fcafcc012691d7
Rafael Castro <rccastrorafael@gmail.com>
2017-08-30 23:29:41 -0300

    :sparkles:  value and paid value is now a integer

--- test/utils/format-date.test.js
+++ test/utils/format-date.test.js
@@ -2,6 +2,6 @@ import formatDate from '../../lib/utils/format-date'
 
 describe('formatDate main functionality', () => {
   it('should format Date for remittance file format', () => {
-    expect(formatDate(new Date(2017, 3, 10),'DDMMYY')).toEqual('100417')
+    expect(formatDate(new Date(2017, 3, 10), 'DDMMYY')).toEqual('100417')
   })
 })
