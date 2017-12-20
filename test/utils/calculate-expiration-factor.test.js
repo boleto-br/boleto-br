fd499bd3feacf1ecfee722c9956639afec9cf6da
Rafael Castro <rccastrorafael@gmail.com>
2017-12-20 12:37:22 -0200

    :bug: windows 7 - 32 bits causing problem in date diff, switch for moment

--- test/utils/calculate-expiration-factor.test.js
+++ test/utils/calculate-expiration-factor.test.js
@@ -4,6 +4,7 @@ describe('calculateExpirationFactor main funcionality', () => {
   it('calculate the expiration factor', () => {
     expect(calculateExpirationFactor(new Date(2017, 6, 10))).toBe('7216')
     expect(calculateExpirationFactor(new Date(1998, 6, 10))).toBe('0276')
+    expect(calculateExpirationFactor(new Date(2017, 11, 4))).toBe('7363')
   })
 
   it('should handle expiration dates prior to 1997', () => {
