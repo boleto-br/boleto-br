23cba4d8835374beef6dfade941d1de5d8d1e57c
Fabrício Doi <fabricio.doi@gmail.com>
2025-03-10 19:23:40 -0300

    tests for 22-feb-2025 new rule

--- test/utils/calculate-expiration-factor.test.js
+++ test/utils/calculate-expiration-factor.test.js
@@ -7,6 +7,13 @@ describe('calculateExpirationFactor main funcionality', () => {
     expect(calculateExpirationFactor(new Date(2017, 11, 4))).toBe('7363')
   })
 
+  it('calculateExpirationFactor after 2025-02-22', () => {
+    expect(calculateExpirationFactor(new Date(2026, 1, 21))).toBe('1364')
+    expect(calculateExpirationFactor(new Date(2025, 2, 21))).toBe('1027')
+    expect(calculateExpirationFactor(new Date(2025, 1, 22))).toBe('1000')
+    expect(calculateExpirationFactor(new Date(2049, 9, 13))).toBe('9999')
+  })
+
   it('should handle expiration dates prior to 1997', () => {
     expect(calculateExpirationFactor(new Date(0))).toBe('0000')
     expect(calculateExpirationFactor(new Date(1997, 6, 10))).toBe('0000')
